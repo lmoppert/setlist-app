a5f44c2c949d85172c1b85ca6972744140ee544e
Lutz Moppert <lutz@moppert.de>
2026-05-08 15:26:41 +0200

    Debug versuch

--- docker-entrypoint.sh
+++ docker-entrypoint.sh
@@ -1,6 +1,11 @@
 #!/bin/sh
 set -e
 
+echo "Checking secret..."
+ls -ls /run/secrets
+
 export DATABASE_URL="$(cat /run/secrets/database_url)"
+
+echo "DATABASE_URL is set"
 
 exec "$@"
--- start.sh
+++ start.sh
@@ -1,8 +1,6 @@
 #!/bin/sh
 set -e
 
-cd apps/api
 npx prisma migrate deploy
-cd ../..
 
 exec node main.js
